42bb1b0db450cbf2cbf0fffc69672f6741083d5a
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-16 14:58:36 +0200

    Delete tests/test_display_molecule_2d.py

--- tests/test_display_molecule_2d.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from Code_without_interface import display_molecule_2d
-
-def test_display_molecule_2d_found():
-    name_molecule = "123"
-    result = display_molecule_2d(name_molecule)
-    assert result == "Error", "Impossible to convert the SMILES into a molecule."
