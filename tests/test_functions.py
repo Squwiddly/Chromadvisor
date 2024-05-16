4f1cc35ccc3fe45f7b1044aaf57c07e0ae5095cf
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-16 14:58:16 +0200

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -19,6 +19,15 @@ def test_get_smiles_nothing():
     assert "An error occurred" in result
 
 ########################################################################################################
+#test the function display_molecule_2d
+from Code_without_interface import display_molecule_2d
+
+def test_display_molecule_2d_found():
+    name_molecule = "123"
+    result = display_molecule_2d(name_molecule)
+    assert result == "Error", "Impossible to convert the SMILES into a molecule."
+
+########################################################################################################
 #test the find_functional_groups function
 from Chromadvisor_without_interface import find_functional_groups
 
