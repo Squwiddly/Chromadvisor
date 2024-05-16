8ab744585baaf3095cd40b93b002cf8cdd4153aa
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-16 11:30:14 +0200

    Update and rename test_example_module.py to test_get_smiles.py

---
+++ tests/test_get_smiles.py
@@ -0,0 +1,18 @@
+from Chromadvisor_without_interface import get_smiles
+
+# Test the function
+
+def test_get_smiles_found():
+    molecule_name = "water"
+    result = get_smiles(molecule_name)
+    assert result == "O"
+
+def test_get_smiles_not_found():
+    molecule_name = "nonexistent_molecule"
+    result = get_smiles(molecule_name)
+    assert result == "Molecule not found. Please try another name."
+
+def test_get_smiles_nothing():
+    molecule_name = ""
+    result = get_smiles(molecule_name)
+    assert "An error occurred" in result
