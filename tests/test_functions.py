74a42ed98a85466c3ea65b509bfaa14c9471e385
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-17 14:24:36 +0200

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -25,7 +25,7 @@ from Code_without_interface import display_molecule_2d
 def test_display_molecule_2d_found():
     name_molecule = "123"
     result = display_molecule_2d(name_molecule)
-    assert result == "Error", "Impossible to convert the SMILES into a molecule."
+    assert result == None
 
 ########################################################################################################
 #test the find_functional_groups function
