460823b79d9f3823bd1b44d9157fc8298dc28b6a
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-17 14:22:27 +0200

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -31,25 +31,28 @@ def test_display_molecule_2d_found():
 #test the find_functional_groups function
 from Chromadvisor_without_interface import find_functional_groups
 
-def test_find_functional_groups_carboxylic_acid():
-    smiles = "CC(=O)O"
+def test_find_functional_groups_salicylic_acid():
+    smiles = "C1=CC=C(C(=C1)C(=O)O)O"
     result = find_functional_groups(smiles)
-    assert result.get("carboxylic acid") == {'count': 1} # 'positions': [(1, 2, 3)]}
+    assert result == {'carboxylic acid': {'count': 1, 'positions': (((6, 7, 8),))},
+                      'phenol': {'count': 2, 'positions': ((6, 3, 2), (6, 3, 4))}}
 
-def test_find_functional_groups_amide():
-    smiles = "CC(=O)NC"
+def test_find_functional_groups_propionamide():
+    smiles = "CCC(=O)N"
     result = find_functional_groups(smiles)
-    assert result.get("amide") == {'count': 1} # 'positions': [(2, 3, 4)]}
+    assert result.get("amide") == {'count': 1, 'positions': ((4, 2, 3, 1),)}
 
 def test_find_functional_groups_no_functional_groups():
     smiles = "CCC"
     result = find_functional_groups(smiles)
+
     assert result == {}
 
 def test_find_functional_groups_invalid_smiles():
     smiles = "InvalidSMILES"
     result = find_functional_groups(smiles)
     assert result is None
+
 
 ########################################################################################################
 #test the on_submit function
