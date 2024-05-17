84c4ca4a57a27bd25454835921cd589ef675f0ea
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-17 20:31:17 +0200

    Update __init__.py

--- src/chromadvisor_pack/__init__.py
+++ src/chromadvisor_pack/__init__.py
@@ -3,3 +3,5 @@
 from __future__ import annotations
 
 __version__ = "0.0.1"
+
+from .functions import get_smiles, find_functional_groups, calculate_logp_and_recommend_solvent, display_molecule_2d, generate_3d_structure, on_submit
