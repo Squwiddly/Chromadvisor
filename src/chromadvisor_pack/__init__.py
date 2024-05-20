3fb738fddcfe2bebeefec2bc56dcabf6e4c741b2
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-20 13:43:40 +0200

    Update __init__.py

--- src/chromadvisor_pack/__init__.py
+++ src/chromadvisor_pack/__init__.py
@@ -1,4 +1,9 @@
 """Recommending an eluant mixture based of the log(P) of the desired molecule"""
-
-from .functions_without_interface import get_smiles, find_functional_groups, calculate_logp_and_recommend_solvent, display_molecule_2d
+from .functions_without_interface import (
+    get_smiles,
+    find_functional_groups,
+    calculate_logp_and_recommend_solvent,
+    display_molecule_2d,
+    generate_3d_structure
+)
 __version__ = "0.0.1"
