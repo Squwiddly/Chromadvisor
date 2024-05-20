c350c777fef26e9e877772e702094bed8094fa20
Vera Dias Gomes <160871015+Squwiddly@users.noreply.github.com>
2024-05-20 14:28:21 +0200

    Update test_functions_without_interface.py

--- tests/test_functions_without_interface.py
+++ tests/test_functions_without_interface.py
@@ -11,7 +11,7 @@ def test_get_smiles_found():
 def test_get_smiles_not_found():
     molecule_name = "nonexistent_molecule"
     result = get_smiles(molecule_name)
-    assert result == 'Molecule not found. Please try another name.'
+    assert None == 'Molecule not found. Please try another name.'
 
 def test_get_smiles_nothing():
     molecule_name = ""
